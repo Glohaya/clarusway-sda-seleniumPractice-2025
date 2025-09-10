0f82bbf64c9aa3dc359bee1edb799c7ba0398207
haya <h2000hhh@gmail.com>
2025-09-10 09:12:05 +0300

    1

--- src/test/java/tasks/T304_DynamicListManagement.java
+++ src/test/java/tasks/T304_DynamicListManagement.java
@@ -6,73 +6,64 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
 
 public class T304_DynamicListManagement {
     WebDriver driver;
+    WebDriverWait wait;
 
-
-    //    Task 5: Dynamic List Management
-
-    @Test
-    void ToDoListTest() {
-        driver.get("http://webdriveruniversity.com/To-Do-List/index.html");
-//    Uses Faker to generate 5 random task names
-        Faker faker = new Faker();
-        String[] tasksToAdd ={
-          faker.random().hex(3),
-        faker.random().hex(3),
-        faker.random().hex(3),
-       faker.random().hex(3),
-        faker.random().hex(3)};
-
-//    Adds all tasks to the todo list
-        // Add all tasks to the to-do list using a for loop
-        WebElement inputField = driver.findElement(By.xpath("//input[@placeholder='Add new todo']"));        for (String task : tasksToAdd) {
-            inputField.sendKeys(task);
-            //inputField.submit();
-
-
-        }
-
-
-        // Marks every other task as completed (strikethrough)
-        List<WebElement> tasks = driver.findElements(By.xpath("1"));
-        for (int i = 0; i < tasks.size(); i += 2) {
-            tasks.get(i).click();
-
-        }
-
-//        Deletes all completed tasks
-
-        List<WebElement> deleteButtons = driver.findElements(By.xpath("//ul/li[@class='completed']/span"));
-        for (WebElement deleteButton : deleteButtons) {
-            deleteButton.click();
-        }
-//        Verifies only incomplete tasks remain
-        List<WebElement> remainingTasks = driver.findElements(By.xpath("//ul/li[not(@class='completed')]"));
-        Assertions.assertEquals(2, remainingTasks.size());
-
-    }
     @BeforeEach
     void setUp() {
-        //    Question: Go to "http://webdriveruniversity.com/To-Do-List/index.html" and create a test that:
-
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://webdriveruniversity.com/To-Do-List/index.html");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
+    @Test
+    void toDoListTest() {
+        Faker faker = new Faker();
+        WebElement addTask = driver.findElement(By.xpath("//input[@type='text']"));
+
+        for (int i = 0; i < 5; i++) {
+            String randomTask = faker.lorem().word();
+            addTask.sendKeys(randomTask, Keys.ENTER);
+        }
+
+        List<WebElement> tasks = driver.findElements(By.tagName("li"));
+        for (int i = 0; i < tasks.size(); i++) {
+            if (i % 2 == 1) {
+                WebElement task = tasks.get(i);
+                task.click();
+                wait.until(ExpectedConditions.attributeContains(task, "class", "completed"));
+            }
+        }
+
+        List<WebElement> completedTasks = driver.findElements(By.cssSelector("li.completed"));
+        for (WebElement completedTask : completedTasks) {
+            WebElement deleteButton = completedTask.findElement(By.cssSelector("i.fa-trash"));
+            deleteButton.click();
+            wait.until(ExpectedConditions.stalenessOf(completedTask));
+        }
+
+        List<WebElement> remainingTasks = driver.findElements(By.tagName("li"));
+        for (WebElement remainingTask : remainingTasks) {
+            String classAttribute = remainingTask.getAttribute("class");
+            Assertions.assertFalse(classAttribute.contains("completed"), "There are still completed tasks remaining.");
+        }
+    }
 
     @AfterEach
-    void tearDown() throws InterruptedException {
-        Thread.sleep(6000);
+    void tearDown() {
         driver.quit();
     }
 }
